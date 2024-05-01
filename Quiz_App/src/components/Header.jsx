07fc3ef12665e8e94d3261fb9ef8bacfb20037d9
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-05-01 16:52:53 +0100

    Update Header.jsx

--- Quiz_App/src/components/Header.jsx
+++ Quiz_App/src/components/Header.jsx
@@ -1,9 +1,9 @@
 import styles from "../style.module.css";
-const Header =()=>{
-    return(
-        <div className={styles.Header}>
-            <h1>Quiz App</h1>
-            </div>
-    )
-}
+const Header = () => {
+  return (
+    <div className={styles.Header}>
+      <h1>Quiz App</h1>
+    </div>
+  );
+};
 export default Header;
